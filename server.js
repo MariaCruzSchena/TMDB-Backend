3de58c197f5b508b17706555ea1b515f7e0e9e38
Maria Cruz Schena <mariacruzschena@gmail.com>
2023-04-24 15:13:31 -0300

    fixed cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(volleyball);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "https://tmdb-frontend-ob4fbsgnj-mariacruzschena.vercel.app/",
+  origin: "https://tmdb-frontend-alpha.vercel.app",
   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'], credentials: true,
 }));
 app.get('/', (req, res) => {res.send('INDEX')})
